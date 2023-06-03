59935cf4b1b5f02e6d6d63b96d44ee95617f6302
Fábio Marques <108775609+FabioMarquescbf@users.noreply.github.com>
2023-06-03 14:01:32 -0300

    Rota de Teste Criada

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,17 @@ import fastify from "fastify";
 
 const server = fastify();
 
+
+server.get('/', (request, reply) => {
+    return "Servidor On...."
+})
+
+
 server.listen({ port: 3000 }, (error, address) => {
     if (error) {
         console.error(error);
         process.exit(1);
     } else {
-        console.log('Servidor rodando na porta 3000');
+        console.log('Servidor rodando na porta 3000}');
     }
 });
